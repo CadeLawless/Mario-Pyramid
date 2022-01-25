412ce97a03a9b43c43d2722fbbb86c90ca9ae2a8
Cade Lawless <78225446+CadeLawless@users.noreply.github.com>
2022-01-25 04:18:56 +0000

    "render pyramid to the DOM along with the console"

--- mario.js
+++ mario.js
@@ -1,5 +1,10 @@
-let rows = prompt('How many rows? ');
+let elmt = document.getElementById("construction");
+elmt.remove();
+
+let rows = prompt('How many rows?');
+
 printPyramid(rows);
+
 function printPyramid(height) {
   let str = "#";
   let count = 0;
@@ -8,9 +13,15 @@ function printPyramid(height) {
     count += 1;
     spaces = height - count; 
     for(let i=0; i<spaces; i++){
-      space += " ";
+      space += ".";
     }
     str += '#';
-    console.log(space + str);
+    line = space + str;
+    console.log(line);
+    let rowStr = document.createElement("p");
+    let node = document.createTextNode(`${line}`);
+    rowStr.appendChild(node);
+    let element = document.getElementById("pyramid");
+    element.appendChild(rowStr);
   }
 }
