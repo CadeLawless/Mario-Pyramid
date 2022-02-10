d9e0bf1f20abd5d020f8e11d1902f92b01de42a2
Cade Lawless <78225446+CadeLawless@users.noreply.github.com>
2022-02-10 03:55:35 +0000

    made a slider and select input for the user to customize the pyramid

--- mario.js
+++ mario.js
@@ -1,26 +1,37 @@
-function determineHeightAndThenDrawPyramid(){
+pyramidSymbol();
+
+function slider(){
+  let height = document.getElementById("height");
+  let sliderNum = document.getElementById("slider-num");
+  sliderNum.innerHTML = height.value;
+}
+function determineHeightAndThenDrawPyramid(x, y){
   let clear = document.getElementById("pyramid");
   clear.innerHTML = "";
   let height = document.getElementById("height");
-  printPyramid(height.value);
+  printPyramid(height.value,x);
 }
-function printPyramid(height) {
-  let str = "#";
+function printPyramid(height, x) {
+  let str = x;
   let count = 0;
   for(let i=0; i<height; i++){
     let space = "";
     count += 1;
     spaces = height - count; 
     for(let i=0; i<spaces; i++){
-      space += ".";
+      space += "\xa0";
     }
-    str += '#';
+    str += x;
     line = space + str;
-    console.log(line);
     let rowStr = document.createElement("p");
     let node = document.createTextNode(`${line}`);
     rowStr.appendChild(node);
     let element = document.getElementById("pyramid");
     element.appendChild(rowStr);
   }
+}
+
+function pyramidSymbol(){
+  let symb = document.getElementById("symbol");
+  determineHeightAndThenDrawPyramid(symb.value);
 }
