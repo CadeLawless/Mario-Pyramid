3ca805108c1cc7a08332e67647950fbef0bab480
Cade Lawless <78225446+CadeLawless@users.noreply.github.com>
2022-03-16 23:56:11 +0000

    added validation

--- mario.js
+++ mario.js
@@ -1,37 +1,106 @@
-pyramidSymbol();
+var heightElem = document.getElementById("height");
+var formElem = document.getElementById("draw-form");
 
-function slider(){
-  let height = document.getElementById("height");
-  let sliderNum = document.getElementById("slider-num");
-  sliderNum.innerHTML = height.value;
-}
-function determineHeightAndThenDrawPyramid(x, y){
-  let clear = document.getElementById("pyramid");
-  clear.innerHTML = "";
-  let height = document.getElementById("height");
-  printPyramid(height.value,x);
-}
-function printPyramid(height, x) {
-  let str = x;
-  let count = 0;
-  for(let i=0; i<height; i++){
-    let space = "";
-    count += 1;
-    spaces = height - count; 
-    for(let i=0; i<spaces; i++){
-      space += "\xa0";
+// set a handler function for the form's submission event
+formElem.onsubmit = function(event) {
+
+    // QUIZ
+    // what happens if we don't do this?
+    event.preventDefault();
+
+    // QUIZ
+    // what happens if we don't do this?
+    clearError();
+
+    // figure out the height the user typed
+    heightStr = heightElem.value;
+
+    // TODO 1
+    // if they didn't type anything at all, give a different error message,
+    // something like "Please provide a height"
+    if (heightStr == "") {
+      displayError("Please provide a height.");
+      return;
     }
-    str += x;
-    line = space + str;
-    let rowStr = document.createElement("p");
-    let node = document.createTextNode(`${line}`);
-    rowStr.appendChild(node);
-    let element = document.getElementById("pyramid");
-    element.appendChild(rowStr);
-  }
+
+    // convert the string to an int
+    height = parseInt(heightStr);
+
+    // if the height is not-a-number, yell at them and exit early
+    // TODO 2
+    // negative numbers and zero should also be rejected here
+    if (isNaN(height) || height <= 0) {
+        displayError("That's not a valid height.");
+        return;
+    }
+
+    // if the height is absurdly tall, yell at them and exit early
+    var tooTall = 100;
+    if (height > tooTall) {
+        displayError("Are you crazy? I can't build a pyramid that tall.");
+        return;
+    }
+
+    // draw pyramid with the specified height
+    drawPyramid(height);
 }
 
-function pyramidSymbol(){
-  let symb = document.getElementById("symbol");
-  determineHeightAndThenDrawPyramid(symb.value);
+
+/**
+ * displayError
+ *
+ * Displays an error message on the text input, and colors it red
+ */
+function displayError(message) {
+    heightElem.className = "invalid-field";
+    document.querySelector(".error-message").innerHTML = message;
+}
+
+
+/*
+ * clearError
+ *
+ * Undisplays the error message and removes the red CSS style
+ */
+function clearError() {
+    // TODO 3
+    // implement this function.
+  heightElem.classList.remove("invalid-field");
+  document.querySelector(".error-message").innerHTML = "";
+}
+
+
+
+/**
+ * drawPyramid
+ *
+ * Renders, in the HTML document, a Mario pyramid of the specified height
+ */
+function drawPyramid(height) {
+
+    // first, clear the old content
+    document.getElementById("pyramid").innerHTML = "";
+
+    // for each row....
+    for (var row = 0; row < height; row++) {
+
+        // figure out number of bricks and spaces
+        var numBricks = row + 2;
+        var numSpaces = height - row - 1;
+
+        // build up a string for this row
+        var rowStr = "";
+        for (var i = 0; i < numSpaces; i++) {
+            var spaceChar = "&nbsp"; // this is the HTML encoding for a space " "
+            rowStr += spaceChar;
+        }
+        for (var i = 0; i < numBricks; i++) {
+            rowStr += "#";
+        }
+
+        // make a <p> element for this row, and insert it into the #pyramid container
+        rowElem = document.createElement("p");
+        rowElem.innerHTML = rowStr;
+        document.getElementById("pyramid").appendChild(rowElem);
+    }
 }
